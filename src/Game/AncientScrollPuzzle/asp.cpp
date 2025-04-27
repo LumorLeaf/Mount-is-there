cbeb5851963aa644e759290c130cf82befa001be
lumor <2784019182@qq.com>
2025-04-27 22:57:24 +0800

    unstable asp4

--- src/Game/AncientScrollPuzzle/asp.cpp
+++ src/Game/AncientScrollPuzzle/asp.cpp
@@ -1,6 +1,16 @@
 #include"asp.h"
 #include<iostream>
-#include<random>
+
+#include<random>//创建随机数
+
+#include<vector>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <locale>
+#include <windows.h>//实现选择诗歌
+
+using namespace std;
 
 int random_circle_number()
 {
@@ -47,78 +57,110 @@ int random_colb()
 
 void problem::operow(int a, int b)//exchange row a with row b
 {
-    if (a <= 0 || a > 4 || b <= 0 || b > 4) // inspect available number
+    if (a <= 0 || a > row || b <= 0 || b > row) // inspect available number
     {
         std::wcout << L"Invalid row indices" << std::endl;
         return;
     }
 
-    for (int i = 0; i < 5; i++)//operate Scroll
+    //for (int i = 0; i < 5; i++)//operate Scroll
+    //{
+    //    wchar_t temp = Scroll[a - 1][i];
+    //    Scroll[a - 1][i] = Scroll[b - 1][i];
+    //    Scroll[b - 1][i] = temp;
+    //}
+    for (int j = 0; j < (row + 1); j++)//operate Scroll
     {
-        wchar_t temp = Scroll[a - 1][i];
-        Scroll[a - 1][i] = Scroll[b - 1][i];
-        Scroll[b - 1][i] = temp;
+        wchar_t tempp = 0;
+        tempp = Scroll[(a - 1) * (row+1) + j];
+        Scroll[(a - 1) * (row+1) + j] = Scroll[(b - 1) * (row + 1) + j];
+        Scroll[(b - 1) * (row + 1) + j] = tempp;
     }
 
-    for (int j = 0; j < 5; j++)//operate Scrollmirror
+    for (int j = 0; j < (row + 1); j++)//operate Scrollmirror
     {
         int tempp = 0;
-        tempp = Scrollmirror[(a - 1) * 5 + j];
-        Scrollmirror[(a - 1) * 5 + j] = Scrollmirror[(b - 1) * 5 + j];
-        Scrollmirror[(b - 1) * 5 + j] = tempp;
+        tempp = Scrollmirror[(a - 1) * (row + 1) + j];
+        Scrollmirror[(a - 1) * (row + 1) + j] = Scrollmirror[(b - 1) * (row + 1) + j];
+        Scrollmirror[(b - 1) * (row + 1) + j] = tempp;
     }
 }
 
 void problem::opecol(int a, int b)//exchange column a with col b
 {
-    if (a <= 0 || a > 5 || b <= 0 || b > 5) // inspect available number
+    if (a <= 0 || a > col || b <= 0 || col > 5) // inspect available number
     {
         std::wcout << L"Invalid column indices" << std::endl;
         return;
     }
 
-    for (int i = 0; i < 4; i++)//operate Scroll
+    //for (int i = 0; i < 4; i++)//operate Scroll
+    //{
+    //    wchar_t temp = Scroll[i][a - 1];
+    //    Scroll[i][a - 1] = Scroll[i][b - 1];
+    //    Scroll[i][b - 1] = temp;
+    //}
+    for (int j = 0; j < col; j++)//operate Scroll
     {
-        wchar_t temp = Scroll[i][a - 1];
-        Scroll[i][a - 1] = Scroll[i][b - 1];
-        Scroll[i][b - 1] = temp;
+        wchar_t tempp = 0;
+        tempp = Scroll[(a - 1) * col + j];
+        Scroll[(a - 1) * col + j] = Scroll[(b - 1) * col + j];
+        Scroll[(b - 1) * col + j] = tempp;
     }
 
-    for (int j = 0; j < 4; j++)//operate Scrollmirror
+    for (int j = 0; j < col; j++)//operate Scrollmirror
     {
         int tempp = 0;
-        tempp = Scrollmirror[a - 1 + j * 5];
-        Scrollmirror[a - 1 + j * 5] = Scrollmirror[b - 1 + j * 5];
-        Scrollmirror[b - 1 + j * 5] = tempp;
+        tempp = Scrollmirror[(a - 1) * col + j];
+        Scrollmirror[(a - 1) * col + j] = Scrollmirror[(b - 1) * col + j];
+        Scrollmirror[(b - 1) * col + j] = tempp;
     }
 }
 
 void problem::print(void)
 {
     std::wcout.imbue(std::locale(""));//for cout chinese character
-    for (int i = 0; i < 4; i++)//Scroll
+    //for (int i = 0; i < 4; i++)//Scrolllast
+    //{
+    //    for (int j = 0; j < 5; j++)
+    //    {
+    //        std::wcout << Scroll[i] << L" ";
+    //    }
+    //    std::wcout << "\n";
+    //}
+
+    for (int i = 0; i < row*col; i++)//Scroll
     {
-        for (int j = 0; j < 5; j++)
+        std::wcout << Scroll[i] << L" ";
+        for (int j = 0; j < row; j++)
         {
-            std::wcout << Scroll[i][j] << L" ";
+            if (i == j*col-1)
+            {
+                std::cout << std::endl;
+            }
         }
-        std::wcout << "\n";
     }
 
-    for (int i = 0; i < 20; i++)//mirror
+    std::cout << std::endl;
+
+    for (int i = 0; i < row*col; i++)//mirror
     {
         std::cout << Scrollmirror[i] << " ";
-        if (i == 4 || i == 9 || i == 14 || i == 19)
+        for (int j = 0; j < row; j++)
         {
-            std::cout << std::endl;
+            if (i==j*col-1)
+            {
+                std::wcout << std::endl;
+            }
         }
     }
+
 }
 
 bool problem::inspect()
 {
     int inspectnumber = 0;
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < row*col; i++)
     {
         if (Scrollmirror[i] == i + 1)
         {
@@ -126,7 +168,7 @@ bool problem::inspect()
         }
     }
 
-    if (inspectnumber == 20)
+    if (inspectnumber == row*col)
     {
         std::cout << "You win!!!!!!!" << std::endl;
         return true;
@@ -141,6 +183,7 @@ bool problem::inspect()
 
 void problem::generate_problem()
 {
+
     for (int i = 0; i < random_circle_number(); i++)
     {
         int ran = random_row_or_col();
@@ -157,6 +200,8 @@ void problem::generate_problem()
 
 void problem::play()
 {
+    choose_poem(1);
+
     generate_problem();
     print();
 
@@ -203,28 +248,28 @@ void problem::play()
 void problem::hint()
 {
     bool finish = false;
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < row; i++)
     {
         if (finish == true)
         {
             break;
         }
 
-        for (int j = 0; j < 20; j++)
+        for (int j = 0; j < row; j++)
         {
             if (finish == true)
             {
                 break;
             }
 
-            int row = 0;
-            if (i * 5 + 1 == Scrollmirror[j])
+            int row2 = 0;
+            if (i * (row+1) + 1 == Scrollmirror[j])
             {
-                row = j / 5 + 1;
-                if (i + 1 != row)
+                row2 = j / (row+1) + 1;
+                if (i + 1 != row2)
                 {
-                    problem::operow(i + 1, row);
-                    std::cout << "row:" << i + 1 << " " << row << std::endl;
+                    problem::operow(i + 1, row2);
+                    std::cout << "row:" << i + 1 << " " << row2 << std::endl;
                     problem::print();
                     finish = true;
                 }
@@ -234,31 +279,31 @@ void problem::hint()
                 }
             }
         }
-       
+
     }
-    //col
-    for (int i = 0; i < 5; i++)
+    //col2
+    for (int i = 0; i < col; i++)
     {
         if (finish == true)
         {
             break;
         }
 
-        for (int j = 0; j < 20; j++)
+        for (int j = 0; j < row*col; j++)
         {
             if (finish == true)
             {
                 break;
             }
 
-            int col = 0;
+            int col2 = 0;
             if (i == Scrollmirror[j] - 1)
             {
-                col = j % 5 + 1;
-                if (i + 1 != col) 
+                col2 = j % col + 1;
+                if (i + 1 != col2)
                 {
-                    problem::opecol(i + 1, col);
-                    std::cout << "col:" << j + 1 << " " << col << std::endl;
+                    problem::opecol(i + 1, col2);
+                    std::cout << "col:" << j + 1 << " " << col2 << std::endl;
                     problem::print();
                     finish = true;
                 }
@@ -273,35 +318,121 @@ void problem::hint()
 
 void problem::answer()
 {
-    //row
+    //row1
     for (int i = 0; i < 4; i++)
     {
         for (int j = 0; j < 20; j++)
         {
-            int row = 0;
+            int row1 = 0;
             if (i * 5 + 1 == Scrollmirror[j])
             {
-                row = j / 5 + 1;
-                problem::operow(i + 1, row);
-                std::cout << "row:" << i + 1 << " " << row << std::endl;
+                row1 = j / 5 + 1;
+                problem::operow(i + 1, row1);
+                std::cout << "row:" << i + 1 << " " << row1 << std::endl;
                 problem::print();
             }
         }
     }
 
-    //col
+    //col1
     for (int i = 0; i < 5; i++)
     {
         for (int j = 0; j < 20; j++)
         {
-            int col = 0;
+            int col1 = 0;
             if (i == Scrollmirror[j] - 1)
             {
-                col = j % 5 + 1;
-                problem::opecol(i + 1, col);
-                std::cout << "col:" << j + 1 << " " << col << std::endl;
+                col1 = j % 5 + 1;
+                problem::opecol(i + 1, col1);
+                std::cout << "col:" << j + 1 << " " << col1 << std::endl;
                 problem::print();
             }
         }
     }
+}
+
+int problem::choose_poem(int dijishou)
+{
+    // 设置宽字符支持
+    try {
+        std::locale::global(std::locale("")); // 使用系统的默认locale
+    }
+    catch (const exception& e) {
+        wcerr << L"设置locale失败: " << e.what() << endl;
+        return 1;
+    }
+
+    //使用宽字符打开文件
+    wstring filename = L"poem.txt";
+    wifstream file(filename);
+    if (!file.is_open()) {
+        wcerr << L"无法打开文件: " << filename << endl;
+        return 1;
+    }
+
+    //读取方法量
+    wstring line;
+    //int dijishou = 1; //第几首，从1开始
+    int counter = 0;
+    bool read_poem = false;
+
+    //读取的结果量
+    wstring title;
+    int num1 = 0, num2 = 0;
+    vector<wchar_t> content;
+
+    //读取过程
+    while (getline(file, line))
+    {
+        if (line == L"-----")
+        {
+            counter++;
+            if (counter == dijishou)
+            {
+                read_poem = true; // 进入目标诗的读取模式
+            }
+            else if (counter > dijishou)
+            {
+                read_poem = false; // 超过目标诗，退出模式
+                break;
+            }
+        }
+        else if (read_poem)
+        {
+            if (title.empty())
+            { // 第一行是标题
+                title = line;
+            }
+            else if (num1 == 0 && num2 == 0)
+            { // 第二行是数字
+                // 解析数字行
+                size_t space_pos = line.find(L' ');
+                num1 = stoi(line.substr(0, space_pos));
+                num2 = stoi(line.substr(space_pos + 1));
+            }
+            else
+            { // 其余行是诗句内容
+                for (wchar_t ch : line)
+                {
+                    content.push_back(ch); // 将每个宽字符存入数组
+                    wchar_t n = L'\n';
+                    if (ch !=n )
+                    {
+                        content.push_back(ch);
+                    }
+                }
+            }
+        }
+
+        row = num1;
+        col = num2;
+        //for (int i = 1; i < num1 * num2; i++)
+        //{
+        //    Scrollmirror.push_back(i);
+        //}
+
+    }
+
+    file.close();
+    return 0;
 }
--- src/Game/AncientScrollPuzzle/asp.h
+++ src/Game/AncientScrollPuzzle/asp.h
@@ -1,13 +1,22 @@
 #ifndef ASP_H
 #define ASP_H
 
+#include<string>
+#include<vector>
+
 class problem
 {
 private:
-    wchar_t Scroll[4][5] = { L'床',L'前',L'明',L'月',L'光',L'疑',L'似',L'地',L'上',L'霜',L'举',L'头',L'望',L'明',L'月',L'低',L'头',L'思',L'故',L'乡' };
+    //std::vector<wchar_t> Scroll;
+    //std::vector<int> Scrollmirror;
+    int row = 0;
+    int col = 0;
+    std::string title = "title";
+    wchar_t Scroll[100] = {L'床',L'前',L'明',L'月',L'光',L'疑',L'似',L'地',L'上',L'霜',L'举',L'头',L'望',L'明',L'月',L'低',L'头',L'思',L'故',L'乡'};
     int Scrollmirror[20] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };
 
 public:
+
     //operation
     void operow(int a, int b);
     void opecol(int a, int b);
@@ -29,6 +38,9 @@ public:
 
     //answer
     void answer();
+
+    //choose poem
+    int choose_poem(int dijishou);
 };
 
 
